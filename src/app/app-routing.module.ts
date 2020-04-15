45b788535bec94465a29eb333f3c31d2d0ccd117
Rahulnsharma50 <djrahul8130565174@gmail.com>
2020-04-15 08:28:47 +0530

    Intial commit Adding Bulma CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'About', component: AboutPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
